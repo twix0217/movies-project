cfa5de660b81411a68538f88c9b12f3ff451ca13
twix217 <hussainbarakat02@gmail.com>
2024-07-30 15:45:46 +0300

    Push2

--- controllers/community.js
+++ controllers/community.js
@@ -7,7 +7,7 @@ router.get('/', async (req, res) => {
     try {
         // Find all users and populate their movies
         const users = await User.find().populate('movies');
-        res.render('community/index', { users }); // Use the .ejs extension only if the view engine is not set to automatically resolve file extensions
+        res.render('community/index.ejs', { users });
     } catch (error) {
         console.log(error);
         res.redirect('/');
@@ -17,10 +17,18 @@ router.get('/', async (req, res) => {
 // Route to display a single movie's details
 router.get('/movies/:movieId', async (req, res) => {
     try {
-        // Find the movie by ID
+        console.log('Movie ID:', req.params.movieId); // Log the movie ID
         const user = await User.findOne({ 'movies._id': req.params.movieId }).populate('movies');
+        if (!user) {
+            console.log('User with the specified movie not found');
+            return res.status(404).send('User with the specified movie not found');
+        }
         const movie = user.movies.id(req.params.movieId);
-        res.render('community/show', { movie, user }); // Use the .ejs extension only if needed
+        if (!movie) {
+            console.log('Movie not found');
+            return res.status(404).send('Movie not found');
+        }
+        res.render('community/show.ejs', { movie, user });
     } catch (error) {
         console.log(error);
         res.redirect('/');
